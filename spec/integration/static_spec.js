f51a8a91f96168c4b0d00aa8cd93cf02f3134cb1
Ryan <ryanmwb@gmail.com>
2018-11-05 16:27:03 -0500

    working on intro to express exercise

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,16 +1,29 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost3000/";
+const base = "http://localhost:3000/";
+const base2 = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
-    describe("GET /", () => {
-        it("should return status code 200", (done) => {
-            request.get(base, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
 
-                done();
-            });
+//#1
+  describe("GET /", () => {
+
+//#2
+    it("should return status code 200", (done) => {
+//#3
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+//#4
+        done();
+      });
+    });
+    it("should return status code 200.  Should accept get request to /marco", (done) => {
+        request.get(base2, (err, res, body) => {
+            expect(res.statusCode).toBe(200)
+
+            done();
         });
     });
+  });
 });
 
