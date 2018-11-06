e60d58f6bfb7e0755186a69eb9df8a570208ed43
Ryan <ryanmwb@gmail.com>
2018-11-06 17:16:50 -0500

    moved server.listen towards bottom of server.js file

--- src/server.js
+++ src/server.js
@@ -4,13 +4,8 @@ const http = require("http");
 const port = normalizePort(process.env.PORT || 3000);
 app.set("port", port);
 
-server.listen(port, () => {
-    console.log("app is running on port " + port);
-});
-
 const server = http.createServer(app);
 
-server.listen(port);
 
 function normalizePort(val) {
   const port = parseInt(val, 10);
@@ -22,6 +17,12 @@ function normalizePort(val) {
   }
   return false;
 }
+
+server.listen(port, () => {
+    console.log("app is running on port " + port);
+});
+
+server.listen(port);
 
 server.on("listening", () => {
   console.log(`server is listening for requests on port ${server.address().port}`);
