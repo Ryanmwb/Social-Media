1f18b759e90d44277d0534048fdebf64f218f43f
Ryan <ryanmwb@gmail.com>
2018-11-08 12:56:12 -0500

    minor changes

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,7 +6,8 @@ describe("routes : static", () => {
     describe("GET /", () => {
         it("should return status code 200 and have 'welcome to bloccit' in the body of the response", () => {
             request.get(base, (err, res, body) => {
-               // expect(res.statusCode).toBe(200);
+                expect(res.statusCode).toBe(200);
+                console.log(`Err: ${err}`)
                 console.log("html Body below...")
                 console.log(body);
                 expect(body).toContain("Welcome 2 Bloccit");
