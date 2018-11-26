92fe49ee10a5c5935e002a858af1b2e93ac0b0a1
Ryan <ryanmwb@gmail.com>
2018-11-26 11:37:36 -0500

    getting error when running > social-media@1.0.0 test C:\Users\Work\Desktop\node.js\social-media > set NODE_ENV=test&& jasmine

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,9 @@ module.exports = {
     init(app){
         const staticRoutes = require("../routes/static");
         const topicRoutes = require("../routes/topics");
+        const postRoutes = require("../controllers/postController");
         app.use(staticRoutes);
         app.use(topicRoutes);
+        app.use(postRoutes);
     }
 }
